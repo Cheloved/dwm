c3982a05a264941fe36ce56809866fbd047a8911
Cheloved <27046990+Cheloved@users.noreply.github.com>
2023-09-20 17:08:09 +0300

    status.sh fixed

--- status.sh
+++ status.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-pgrep -x status.sh > /dev/null && exit
+pgrep -x status.sh > /dev/null || exit
 
 while true; do
     INFO="󰕾 $(pamixer --get-volume)% | "
-    INFO=${INFO}" $(xkb-switch)"
-    INFO=${INFO}" $(date '+%A, %B %-d')"
-    INFO=${INFO}"󱑂 $(date '+%I:%M %p')"
+    INFO=${INFO}" $(xkb-switch) | "
+    INFO=${INFO}" $(date '+%A, %B %-d') | "
+    INFO=${INFO}"󱑂 $(date '+%I:%M %p') | "
     xsetroot -name "${INFO}"
 done
